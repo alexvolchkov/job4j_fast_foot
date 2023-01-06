19dc68671b995b102502fe5fbd9b32b338d31f2a
alexvolchkov <volkaa@mail.ru>
2023-01-06 17:06:21 +0300

    2. Асинхронное взаимодействие [#458497 #371302]

--- notification/src/main/java/ru/job4j/Job4jAuthApplication.java
+++ notification/src/main/java/ru/job4j/Job4jAuthApplication.java
@@ -2,8 +2,12 @@ package ru.job4j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableKafka
+@EnableAsync
 public class Job4jAuthApplication {
 
     public static void main(String[] args) {
