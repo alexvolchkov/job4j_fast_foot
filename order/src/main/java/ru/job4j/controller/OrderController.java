fbbf32741c7c2fcd237e8637f729852ab2ea3c15
alexvolchkov <volkaa@mail.ru>
2023-01-18 22:15:35 +0300

    2. Асинхронное взаимодействие [#458497 #371302]

--- order/src/main/java/ru/job4j/controller/OrderController.java
+++ order/src/main/java/ru/job4j/controller/OrderController.java
@@ -1,9 +1,7 @@
 package ru.job4j.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 import ru.job4j.Order;
 import ru.job4j.Status;
@@ -14,21 +12,16 @@ import ru.job4j.service.OrderService;
 public class OrderController {
     private final OrderService orders;
 
-    @Autowired
-    private KafkaTemplate<Integer, String> template;
-
     public OrderController(OrderService orders) {
         this.orders = orders;
     }
 
     @PostMapping("/")
     public ResponseEntity<Order> createOrder(@RequestBody Order order) {
-        var result = new ResponseEntity<>(
+        return new ResponseEntity<>(
                 orders.createOrder(order),
                 HttpStatus.CREATED
         );
-        template.send("create-order", String.valueOf(order.getId()));
-        return result;
     }
 
     @GetMapping("/status/{id}")
--- order/src/main/java/ru/job4j/service/OrderServiceImpl.java
+++ order/src/main/java/ru/job4j/service/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package ru.job4j.service;
 
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import ru.job4j.Order;
 import ru.job4j.Status;
@@ -8,14 +9,17 @@ import ru.job4j.repository.OrderRepository;
 @Service
 public class OrderServiceImpl implements OrderService {
     private final OrderRepository orders;
+    private final KafkaTemplate<Integer, String> template;
 
-    public OrderServiceImpl(OrderRepository orders) {
+    public OrderServiceImpl(OrderRepository orders, KafkaTemplate<Integer, String> template) {
         this.orders = orders;
+        this.template = template;
     }
 
     @Override
     public Order createOrder(Order order) {
         orders.save(order);
+        template.send("create-order", String.valueOf(order.getId()));
         return order;
     }
 
